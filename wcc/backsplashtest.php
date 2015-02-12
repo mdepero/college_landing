05e0cf70e2126c1a78321810fe205138c1e4690d
Matt DePero <deperomd@gmail.com>
2015-02-12 09:56:18 -0500

    Transfer of sites to github hosting

--- wcc/backsplashtest.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'body.php';
-?>
-<!--ORIGNAL DESIGN BACKUP-->
-<div class="table">
-<div class="tabletop"><img src="tablebgtop.png" /></div>
-<div class="tablebottom"><img src="tablebgbottom.png" /></div>
-<table background="tablebg.jpg" width="1000" cellpadding="5" cellspacing="5">
--- wcc/tools/readme.txt
+++
@@ -1,3 +0,0 @@
-
-Provided is a directory filled with useful files you may use with your account if needed.
-
