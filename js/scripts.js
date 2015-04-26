457501ff729533ba8dae40363ed15cd853847eb3
Matt DePero <deperomd@gmail.com>
2015-04-26 00:42:14 -0400

    Start of resume header

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,12 @@ $(document).ready(function(){
 
 	setInterval(function(){
 	    $(".resume-scroll")
-	    .animate({MarginBottom:"10px"}, 200).animate({MarginBottom:"0px"}, 200) // first
+	    
+	    .animate({MarginBottom:"100px"}, 200);
+
+	    /*.animate({MarginBottom:"10px"}, 200).animate({MarginBottom:"0px"}, 200) // first
 	     .animate({MarginBottom:"6px"}, 200).animate({MarginBottom:"0px"}, 200) // second
-	      .animate({MarginBottom:"4px"}, 200).animate({MarginBottom:"0px"}, 200) // third
+	      .animate({MarginBottom:"4px"}, 200).animate({MarginBottom:"0px"}, 200) // third */
 	},1500);
 
 });
