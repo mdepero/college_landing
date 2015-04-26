dd7944ac7a6e1461c781110841576b56ab2966d3
Matt DePero <deperomd@gmail.com>
2015-04-26 00:39:31 -0400

    Start of resume header

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,6 @@ $(document).ready(function(){
 	    .animate({MarginBottom:"10px"}, 200).animate({MarginBottom:"0px"}, 200) // first
 	     .animate({MarginBottom:"6px"}, 200).animate({MarginBottom:"0px"}, 200) // second
 	      .animate({MarginBottom:"4px"}, 200).animate({MarginBottom:"0px"}, 200) // third
-	},600);
+	},1500);
 
 )};
