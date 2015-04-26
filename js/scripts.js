dd0f3b23431bbaa863681a287b2fa0327ae118ab
Matt DePero <deperomd@gmail.com>
2015-04-26 00:46:37 -0400

    Start of resume header

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,26 @@ $(document).ready(function(){
 	},1500);
 
 });
+
+
+
+
+
+/*
+ * Smooth Scroll provided by css-tricks: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+ * and by Karl Swedberg: http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links
+ */
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
