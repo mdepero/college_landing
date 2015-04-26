539f7043ebd3f69d7875b0f8e2a615376a7f78f9
Matt DePero <deperomd@gmail.com>
2015-04-26 16:58:14 -0400

    building resume

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@
  */
 
 
+options = $.extend({
+    scrollwheel: false,
+    navigationControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    draggable: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+}, options);
+
 
 
 /*
